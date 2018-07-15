e954d70baa20b2ef5d6d714643a668324d064e29
wangdaye <wangdayeeeeee@gmail.com>
2018-07-15 11:36:00 +0800

    2.109

--- app/src/main/java/wangdaye/com/geometricweather/ui/adapter/AboutAdapter.java
+++ app/src/main/java/wangdaye/com/geometricweather/ui/adapter/AboutAdapter.java
@@ -3,6 +3,7 @@ package wangdaye.com.geometricweather.ui.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
