20a1e4292fb21800864c1bc890aa5ffc274f7689
WangDaYeeeeee <wangdayeeeeee@gmail.com>
2019-09-11 19:21:42 +0800

    2.307

--- app/src/main/java/wangdaye/com/geometricweather/location/service/AMapLocationService.java
+++ app/src/main/java/wangdaye/com/geometricweather/location/service/AMapLocationService.java
@@ -109,7 +109,6 @@ public class AMapLocationService extends LocationService {
         return new String[] {
                 Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.READ_PHONE_STATE,
                 Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
         };
--- app/src/main/java/wangdaye/com/geometricweather/location/service/BaiduLocationService.java
+++ app/src/main/java/wangdaye/com/geometricweather/location/service/BaiduLocationService.java
@@ -112,7 +112,6 @@ public class BaiduLocationService extends LocationService {
         return new String[] {
                 Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.READ_PHONE_STATE,
                 Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
         };
