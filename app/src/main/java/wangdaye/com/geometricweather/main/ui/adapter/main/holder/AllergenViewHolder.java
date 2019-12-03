2951a6236ede3e7979b13eeffd7a61aa83a5284f
wangdaye <wangyue971095985@qq.com>
2019-12-03 12:40:35 +0800

    rebuild

--- app/src/main/java/wangdaye/com/geometricweather/main/ui/adapter/main/holder/AllergenViewHolder.java
+++ app/src/main/java/wangdaye/com/geometricweather/main/ui/adapter/main/holder/AllergenViewHolder.java
@@ -1,23 +1,23 @@
 package wangdaye.com.geometricweather.main.ui.adapter.main.holder;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.res.ColorStateList;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.annotation.Px;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.cardview.widget.CardView;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import wangdaye.com.geometricweather.R;
 import wangdaye.com.geometricweather.basic.GeoActivity;
 import wangdaye.com.geometricweather.basic.model.location.Location;
-import wangdaye.com.geometricweather.basic.model.weather.Weather;
+import wangdaye.com.geometricweather.basic.model.option.unit.PollenUnit;
+import wangdaye.com.geometricweather.basic.model.weather.Pollen;
 import wangdaye.com.geometricweather.main.ui.MainColorPicker;
-import wangdaye.com.geometricweather.main.ui.adapter.PollenAdapter;
 import wangdaye.com.geometricweather.resource.provider.ResourceProvider;
 import wangdaye.com.geometricweather.ui.widget.weatherView.WeatherView;
 import wangdaye.com.geometricweather.utils.helpter.IntentHelper;
@@ -27,14 +27,25 @@ public class AllergenViewHolder extends AbstractMainCardViewHolder {
     private CardView card;
     private TextView title;
     private TextView subtitle;
+    
+    private AppCompatImageView grassIcon;
+    private TextView grassTitle;
+    private TextView grassValue;
 
-    private RecyclerView recyclerView;
-    private PollenAdapter adapter;
+    private AppCompatImageView ragweedIcon;
+    private TextView ragweedTitle;
+    private TextView ragweedValue;
 
+    private AppCompatImageView treeIcon;
+    private TextView treeTitle;
+    private TextView treeValue;
+
+    private AppCompatImageView moldIcon;
+    private TextView moldTitle;
+    private TextView moldValue;
+    
     @NonNull private WeatherView weatherView;
-    @Nullable private Weather weather;
-
-    private boolean enable;
+    private PollenUnit unit;
 
     public AllergenViewHolder(@NonNull Activity activity, ViewGroup parent, @NonNull WeatherView weatherView,
                               @NonNull ResourceProvider provider, @NonNull MainColorPicker picker,
@@ -47,42 +58,59 @@ public class AllergenViewHolder extends AbstractMainCardViewHolder {
         this.card = itemView.findViewById(R.id.container_main_pollen);
         this.title = itemView.findViewById(R.id.container_main_pollen_title);
         this.subtitle = itemView.findViewById(R.id.container_main_pollen_subtitle);
-        this.recyclerView = itemView.findViewById(R.id.container_main_pollen_recyclerView);
 
+        this.grassIcon = itemView.findViewById(R.id.container_main_pollen_grassIcon);
+        this.grassTitle = itemView.findViewById(R.id.container_main_pollen_grassTitle);
+        this.grassValue = itemView.findViewById(R.id.container_main_pollen_grassValue);
+        this.ragweedIcon = itemView.findViewById(R.id.container_main_pollen_ragweedIcon);
+        this.ragweedTitle = itemView.findViewById(R.id.container_main_pollen_ragweedTitle);
+        this.ragweedValue = itemView.findViewById(R.id.container_main_pollen_ragweedValue);
+        this.treeIcon = itemView.findViewById(R.id.container_main_pollen_treeIcon);
+        this.treeTitle = itemView.findViewById(R.id.container_main_pollen_treeTitle);
+        this.treeValue = itemView.findViewById(R.id.container_main_pollen_treeValue);
+        this.moldIcon = itemView.findViewById(R.id.container_main_pollen_moldIcon);
+        this.moldTitle = itemView.findViewById(R.id.container_main_pollen_moldTitle);
+        this.moldValue = itemView.findViewById(R.id.container_main_pollen_moldValue);
+        
         this.weatherView = weatherView;
+        this.unit = PollenUnit.PPCM;
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindView(@NonNull Location location) {
-        weather = location.getWeather();
-        assert weather != null;
-
-        enable = true;
+        assert location.getWeather() != null;
 
         card.setCardBackgroundColor(picker.getRootColor(context));
         title.setTextColor(weatherView.getThemeColors(picker.isLightTheme())[0]);
         subtitle.setTextColor(picker.getTextSubtitleColor(context));
 
-        adapter = new PollenAdapter(context, weather, picker, itemAnimationEnabled);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        Pollen pollen = location.getWeather().getDailyForecast().get(0).getPollen();
 
-        itemView.findViewById(R.id.container_main_pollen_titleBar).setOnClickListener(v ->
-                IntentHelper.startAllergenActivity((GeoActivity) context, location));
-    }
+        grassIcon.setSupportImageTintList(ColorStateList.valueOf(
+                Pollen.getPollenColor(itemView.getContext(), pollen.getGrassLevel())
+        ));
+        grassTitle.setText(context.getString(R.string.grass));
+        grassValue.setText(unit.getPollenText(pollen.getGrassIndex()) + " - " + pollen.getGrassDescription());
 
-    @Override
-    public void onEnterScreen() {
-        if (itemAnimationEnabled && enable && weather != null) {
-            adapter.executeAnimation();
-        }
-    }
+        ragweedIcon.setSupportImageTintList(ColorStateList.valueOf(
+                Pollen.getPollenColor(itemView.getContext(), pollen.getRagweedLevel())
+        ));
+        ragweedTitle.setText(context.getString(R.string.ragweed));
+        ragweedValue.setText(unit.getPollenText(pollen.getRagweedIndex()) + " - " + pollen.getRagweedDescription());
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (adapter != null) {
-            adapter.cancelAnimation();
-        }
+        treeIcon.setSupportImageTintList(ColorStateList.valueOf(
+                Pollen.getPollenColor(itemView.getContext(), pollen.getTreeLevel())
+        ));
+        treeTitle.setText(context.getString(R.string.tree));
+        treeValue.setText(unit.getPollenText(pollen.getTreeIndex()) + " - " + pollen.getTreeDescription());
+
+        moldIcon.setSupportImageTintList(ColorStateList.valueOf(
+                Pollen.getPollenColor(itemView.getContext(), pollen.getMoldLevel())
+        ));
+        moldTitle.setText(context.getString(R.string.mold));
+        moldValue.setText(unit.getPollenText(pollen.getMoldIndex()) + " - " + pollen.getMoldDescription());
+
+        itemView.setOnClickListener(v -> IntentHelper.startAllergenActivity((GeoActivity) context, location));
     }
 }
