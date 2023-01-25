9480f08684f3c2d5b65607844e852290878787af
Julien Papasian <julien.papasian@gmail.com>
2023-01-25 10:22:37 +0100

    Languages: add Ind+Cat and sort all languages

--- app/src/main/java/wangdaye/com/geometricweather/common/basic/models/options/appearance/Language.kt
+++ app/src/main/java/wangdaye/com/geometricweather/common/basic/models/options/appearance/Language.kt
@@ -22,30 +22,32 @@ enum class Language(
             Resources.getSystem().configuration.locale
         }
     ),
-    CHINESE("chinese", Locale("zh", "CN")),
-    UNSIMPLIFIED_CHINESE("unsimplified_chinese", Locale("zh", "TW")),
-    ENGLISH_US("english_america", Locale("en", "US")),
-    ENGLISH_UK("english_britain", Locale("en", "GB")),
-    ENGLISH_AU("english_australia", Locale("en", "AU")),
-    TURKISH("turkish", Locale("tr")),
-    FRENCH("french", Locale("fr")),
-    RUSSIAN("russian", Locale("ru")),
+    CATALAN("catalan", Locale("ca")),
+    CZECH("czech", Locale("cs")),
     GERMAN("german", Locale("de")),
-    SERBIAN("serbian", Locale("sr")),
+    ENGLISH_AU("english_australia", Locale("en", "AU")),
+    ENGLISH_UK("english_britain", Locale("en", "GB")),
+    ENGLISH_US("english_america", Locale("en", "US")),
     SPANISH("spanish", Locale("es")),
+    FRENCH("french", Locale("fr")),
+    INDONESIAN("indonesian", Locale("in")),
     ITALIAN("italian", Locale("it")),
-    DUTCH("dutch", Locale("nl")),
     HUNGARIAN("hungarian", Locale("hu")),
+    DUTCH("dutch", Locale("nl")),
+    POLISH("polish", Locale("pl")),
     PORTUGUESE("portuguese", Locale("pt")),
     PORTUGUESE_BR("portuguese_brazilian", Locale("pt", "BR")),
+    ROMANIAN("romanian", Locale("ro")),
     SLOVENIAN("slovenian", Locale("sl", "SI")),
-    ARABIC("arabic", Locale("ar")),
-    CZECH("czech", Locale("cs")),
-    POLISH("polish", Locale("pl")),
-    KOREAN("korean", Locale("ko")),
+    TURKISH("turkish", Locale("tr")),
     GREEK("greek", Locale("el")),
+    RUSSIAN("russian", Locale("ru")),
+    SERBIAN("serbian", Locale("sr")),
+    ARABIC("arabic", Locale("ar")),
+    KOREAN("korean", Locale("ko")),
     JAPANESE("japanese", Locale("ja")),
-    ROMANIAN("romanian", Locale("ro"));
+    CHINESE("chinese", Locale("zh", "CN")),
+    UNSIMPLIFIED_CHINESE("unsimplified_chinese", Locale("zh", "TW"));
 
     val code: String
         get() {
@@ -69,30 +71,32 @@ enum class Language(
         fun getInstance(
             value: String
         ) = when (value) {
-            "chinese" -> CHINESE
-            "unsimplified_chinese" -> UNSIMPLIFIED_CHINESE
-            "english_america" -> ENGLISH_US
-            "english_britain" -> ENGLISH_UK
-            "english_australia" -> ENGLISH_AU
-            "turkish" -> TURKISH
-            "french" -> FRENCH
-            "russian" -> RUSSIAN
+            "catalan" -> CATALAN
+            "czech" -> CZECH
             "german" -> GERMAN
-            "serbian" -> SERBIAN
+            "english_australia" -> ENGLISH_AU
+            "english_britain" -> ENGLISH_UK
+            "english_america" -> ENGLISH_US
             "spanish" -> SPANISH
+            "french" -> FRENCH
+            "indonesian" -> INDONESIAN
             "italian" -> ITALIAN
-            "dutch" -> DUTCH
             "hungarian" -> HUNGARIAN
+            "dutch" -> DUTCH
+            "polish" -> POLISH
             "portuguese" -> PORTUGUESE
             "portuguese_brazilian" -> PORTUGUESE_BR
-            "slovenian" -> SLOVENIAN
-            "arabic" -> ARABIC
-            "czech" -> CZECH
-            "polish" -> POLISH
-            "korean" -> KOREAN
-            "greek" -> GREEK
-            "japanese" -> JAPANESE
             "romanian" -> ROMANIAN
+            "slovenian" -> SLOVENIAN
+            "turkish" -> TURKISH
+            "greek" -> GREEK
+            "russian" -> RUSSIAN
+            "serbian" -> SERBIAN
+            "arabic" -> ARABIC
+            "korean" -> KOREAN
+            "japanese" -> JAPANESE
+            "chinese" -> CHINESE
+            "unsimplified_chinese" -> UNSIMPLIFIED_CHINESE
             else -> FOLLOW_SYSTEM
         }
     }
