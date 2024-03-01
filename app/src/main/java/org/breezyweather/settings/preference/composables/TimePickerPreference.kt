13b943d83a289ae2ee59a392e7e46350ed3c1774
min7-i <97251923+min7-i@users.noreply.github.com>
2024-03-01 17:24:26 +0100

    Center time picker horizontally

--- app/src/main/java/org/breezyweather/settings/preference/composables/TimePickerPreference.kt
+++ app/src/main/java/org/breezyweather/settings/preference/composables/TimePickerPreference.kt
@@ -27,6 +27,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -34,6 +35,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.platform.LocalContext
@@ -142,7 +144,10 @@ fun TimePickerPreferenceView(
                         )
 
                         timePicker
-                    }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentWidth(Alignment.CenterHorizontally)
                 )
             },
             confirmButton = {
