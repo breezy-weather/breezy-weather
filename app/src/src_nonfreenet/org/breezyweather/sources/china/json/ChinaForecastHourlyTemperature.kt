8637f4b1d86ed15878f552db18b8df4b1d80b629
JiunnTarn <aencil@yeah.net>
2024-06-14 20:35:47 +0800

    Fix #1134 - Correct hourly forecast time offset
    Use `forecastHourly.temperature.pubTime` as the baseline time for hour prediction in China source
    
    When `forecastHourly.temperature.pubTime` is null, fallback to current time.

--- app/src/src_nonfreenet/org/breezyweather/sources/china/json/ChinaForecastHourlyTemperature.kt
+++ app/src/src_nonfreenet/org/breezyweather/sources/china/json/ChinaForecastHourlyTemperature.kt
@@ -17,11 +17,11 @@
 package org.breezyweather.sources.china.json
 
 import kotlinx.serialization.Serializable
+import org.breezyweather.common.serializer.DateSerializer
+import java.util.Date
 
 @Serializable
-data class ChinaForecastHourly(
-    val desc: String?,
-    val temperature: ChinaForecastHourlyTemperature?,
-    val weather: ChinaValueListInt?,
-    val wind: ChinaHourlyWind?
+data class ChinaForecastHourlyTemperature(
+    @Serializable(DateSerializer::class) val pubTime: Date?,
+    val value: List<Int>?
 )
