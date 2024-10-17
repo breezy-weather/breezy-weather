2a88ffaef28ef0990811a5756b583c1a8306dd12
min7-i <97251923+min7-i@users.noreply.github.com>
2024-10-17 16:51:30 +0200

    hourly weather dialog

--- app/src/main/java/org/breezyweather/main/dialogs/HourlyWeatherDialog.kt
+++ app/src/main/java/org/breezyweather/main/dialogs/HourlyWeatherDialog.kt
@@ -17,52 +17,116 @@
 package org.breezyweather.main.dialogs
 
 import android.app.Activity
+import android.graphics.drawable.Drawable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.TextView
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import breezyweather.domain.location.model.Location
 import breezyweather.domain.weather.model.Hourly
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import org.breezyweather.R
 import org.breezyweather.common.extensions.currentLocale
 import org.breezyweather.common.extensions.getFormattedMediumDayAndMonth
 import org.breezyweather.common.extensions.getHour
 import org.breezyweather.common.ui.widgets.AnimatableIconView
+import org.breezyweather.domain.location.model.isDaylight
+import org.breezyweather.main.utils.MainThemeColorProvider
 import org.breezyweather.settings.SettingsManager
+import org.breezyweather.theme.compose.BreezyWeatherTheme
 import org.breezyweather.theme.resource.ResourceHelper
 import org.breezyweather.theme.resource.ResourcesProviderFactory
 import java.text.NumberFormat
 
 object HourlyWeatherDialog {
-    fun show(activity: Activity, location: Location, hourly: Hourly) {
+    fun show(
+        activity: Activity,
+        location: Location,
+        hourly: Hourly
+    ) {
         val view = LayoutInflater
             .from(activity)
-            .inflate(R.layout.dialog_weather_hourly, null, false)
-        initWidget(view, hourly)
-        MaterialAlertDialogBuilder(activity)
-            .setTitle(
-                hourly.date.getHour(location, activity)
-                    + " - "
-                    + hourly.date.getFormattedMediumDayAndMonth(location, activity)
-            )
-            .setView(view)
-            .show()
+            .inflate(R.layout.dialog_weather_hourly, activity.findViewById(android.R.id.content), true)
+
+        val composeView = view.findViewById<ComposeView>(R.id.dialog_weather_hourly)
+        val weatherIconView = AnimatableIconView(activity.baseContext)
+
+        val weatherText = getWeatherText(view, hourly)
+        //val weatherIcon = getWeatherIcon(hourly)
+        val dialogOpenState = mutableStateOf(true)
+
+        composeView.setContent {
+            BreezyWeatherTheme(lightTheme = MainThemeColorProvider.isLightTheme(
+                activity, daylight = location.isDaylight
+            )) {
+                if (dialogOpenState.value) {
+                    AlertDialog(
+                        onDismissRequest = {
+                            dialogOpenState.value = false
+                        },
+                        confirmButton = {},
+                        dismissButton = {},
+                        icon = {
+                            AndroidView(
+                                modifier = Modifier.size(48.dp),
+                                factory = {
+                                    weatherIconView.apply {
+                                        val provider = ResourcesProviderFactory.newInstance
+                                        val weatherCode = hourly.weatherCode
+                                        val daytime = hourly.isDaylight
+                                        if (weatherCode != null) {
+                                            setAnimatableIcon(
+                                                ResourceHelper.getWeatherIcons(
+                                                    provider,
+                                                    weatherCode,
+                                                    daytime
+                                                ),
+                                                ResourceHelper.getWeatherAnimators(
+                                                    provider,
+                                                    weatherCode,
+                                                    daytime
+                                                )
+                                            )
+                                        }
+                                        setOnClickListener { startAnimators() }
+                                    }
+                                }
+                            )
+                            /*
+                            weatherIcon?.toBitmapOrNull()?.asImageBitmap()?.let {
+                                Icon(
+                                    bitmap = it,
+                                    tint = Color.Unspecified,
+                                    contentDescription = null,
+                                    modifier = Modifier.size(48.dp)
+                                )
+                            }*/
+                        },
+                        title = { Text(hourly.date.getHour(location, activity)
+                                + " - "
+                                + hourly.date.getFormattedMediumDayAndMonth(location, activity)) },
+                        text = {
+                            Text(
+                                text = weatherText,
+                                modifier = Modifier.clickable {
+                                    weatherIconView.startAnimators()
+                                }
+                            )
+                        }
+                    )
+                }
+            }
+        }
     }
 
-    private fun initWidget(view: View, hourly: Hourly) {
-        val provider = ResourcesProviderFactory.newInstance
-        val weatherIcon = view.findViewById<AnimatableIconView>(R.id.dialog_weather_hourly_icon)
-        view.findViewById<View>(R.id.dialog_weather_hourly_weatherContainer)
-            .setOnClickListener { weatherIcon.startAnimators() }
-        val weatherCode = hourly.weatherCode
-        val daytime = hourly.isDaylight
-        if (weatherCode != null) {
-            weatherIcon.setAnimatableIcon(
-                ResourceHelper.getWeatherIcons(provider, weatherCode, daytime),
-                ResourceHelper.getWeatherAnimators(provider, weatherCode, daytime)
-            )
-        }
-        val weatherText = view.findViewById<TextView>(R.id.dialog_weather_hourly_text)
+    private fun getWeatherText(view: View, hourly: Hourly): String {
         val settings = SettingsManager.getInstance(view.context)
         val temperatureUnit = settings.temperatureUnit
         val precipitationUnit = settings.precipitationUnit
@@ -102,6 +166,15 @@ object HourlyWeatherDialog {
                     }.format(hourly.precipitationProbability!!.total!!.div(100.0))
                 )
         }
-        weatherText.text = builder.toString()
+        return builder.toString()
+    }
+
+    private fun getWeatherIcon(hourly: Hourly): Drawable? {
+        val provider = ResourcesProviderFactory.newInstance
+
+        return hourly.weatherCode?.let {
+            ResourceHelper.getWeatherIcon(provider,
+                it, hourly.isDaylight)
+        }
     }
 }
