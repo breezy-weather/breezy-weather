6cc8e4a5525e3970f400ae19e5cba12ecefc0c56
min7-i <97251923+min7-i@users.noreply.github.com>
2024-12-02 17:29:14 +0100

    Fix #1474 - Handle multiple refresh errors
    If more than a single error occurred during a weather data refresh, a generic snackbar is shown with an action to show error details in a dialog. Otherwise, the current behavior is kept and a snackbar with the error is directly shown.

---
+++ app/src/main/java/org/breezyweather/common/extensions/ModifierExtensions.kt
@@ -0,0 +1,33 @@
+/**
+ * This file is part of Breezy Weather.
+ *
+ * Breezy Weather is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, version 3 of the License.
+ *
+ * Breezy Weather is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Breezy Weather. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.breezyweather.common.extensions
+
+import androidx.compose.ui.Modifier
+
+/**
+ * Source: ProAndroidDev
+ * https://proandroiddev.com/jetpack-compose-tricks-conditionally-applying-modifiers-for-dynamic-uis-e3fe5a119f45
+ */
+inline fun Modifier.conditional(
+    condition: Boolean,
+    ifTrue: Modifier.() -> Modifier,
+    ifFalse: Modifier.() -> Modifier = { this },
+): Modifier = if (condition) {
+    then(ifTrue(Modifier))
+} else {
+    then(ifFalse(Modifier))
+}
