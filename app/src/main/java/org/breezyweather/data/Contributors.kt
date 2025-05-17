8918b1ffd9fefac79bb19e92bf7b51bed978a5c0
Julien Papasian <julien.papasian@gmail.com>
2025-05-17 20:07:22 +0200

    Post-translation update

--- app/src/main/java/org/breezyweather/data/Contributors.kt
+++ app/src/main/java/org/breezyweather/data/Contributors.kt
@@ -261,6 +261,7 @@ val appTranslators = arrayOf(
     TranslatorItem(arrayOf("ru"), "Егор Ермаков", weblate = "creepen"),
     TranslatorItem(arrayOf("ru"), "TenchMaviatorius2759", github = "TenchMaviatorius2759"),
     TranslatorItem(arrayOf("ru"), "mak7im01", github = "mak7im01"),
+    TranslatorItem(arrayOf("ru"), "Tim", weblate = "dlee"),
     TranslatorItem(arrayOf("sk"), "Kuko", weblate = "kuko7"),
     TranslatorItem(arrayOf("sk", "cs"), "Viliam Geffert", github = "vgeffer"),
     TranslatorItem(arrayOf("sk"), "aasami", weblate = "aasami"),
