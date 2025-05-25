3c82df27f14fef77bae72ef83aae54dd766d4cba
Julien Papasian <julien.papasian@gmail.com>
2025-05-25 10:16:07 +0200

    Update SDK to 36

--- buildSrc/src/main/kotlin/breezy/buildlogic/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/breezy/buildlogic/AndroidConfig.kt
@@ -4,9 +4,9 @@ import org.gradle.api.JavaVersion as GradleJavaVersion
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget as KotlinJvmTarget
 
 object AndroidConfig {
-    const val COMPILE_SDK = 35
+    const val COMPILE_SDK = 36
     const val MIN_SDK = 21
-    const val TARGET_SDK = 35
+    const val TARGET_SDK = 36
 
     // https://youtrack.jetbrains.com/issue/KT-66995/JvmTarget-and-JavaVersion-compatibility-for-easier-JVM-version-setup
     val JavaVersion = GradleJavaVersion.VERSION_1_8
