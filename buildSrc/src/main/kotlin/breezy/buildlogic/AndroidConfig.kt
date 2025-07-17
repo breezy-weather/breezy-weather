5e82a131a0fc5148607c8f12d45ef44fef2a53c6
Julien Papasian <julien.papasian@gmail.com>
2025-07-17 18:22:13 +0200

    Update dependencies

--- buildSrc/src/main/kotlin/breezy/buildlogic/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/breezy/buildlogic/AndroidConfig.kt
@@ -7,6 +7,7 @@ object AndroidConfig {
     const val COMPILE_SDK = 36
     const val MIN_SDK = 21
     const val TARGET_SDK = 36
+    const val BUILD_TOOLS = "35.0.1"
 
     // https://youtrack.jetbrains.com/issue/KT-66995/JvmTarget-and-JavaVersion-compatibility-for-easier-JVM-version-setup
     val JavaVersion = GradleJavaVersion.VERSION_1_8
--- buildSrc/src/main/kotlin/breezy/buildlogic/ProjectExtensions.kt
+++ buildSrc/src/main/kotlin/breezy/buildlogic/ProjectExtensions.kt
@@ -15,6 +15,7 @@ val Project.libs get() = the<LibrariesForLibs>()
 internal fun Project.configureAndroid(commonExtension: CommonExtension<*, *, *, *, *, *>) {
     commonExtension.apply {
         compileSdk = AndroidConfig.COMPILE_SDK
+        buildToolsVersion = AndroidConfig.BUILD_TOOLS
 
         defaultConfig {
             minSdk = AndroidConfig.MIN_SDK
